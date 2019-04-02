9a3c59b3f0ba77b7d1b3dd21eec62e03db118438
khaledalhariri200 <47532475+khaledalhariri200@users.noreply.github.com>
2019-04-02 10:52:53 -0400

    SUFF

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		int num1=in.nextInt();
+		int counter=1;
+		while (counter<=num1){
+			System.out.println(counter);
+			counter++;
+
+		}
 	}
 
 }
+
--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -5,7 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// Read in two numbers. Add up all the values between them
 		Scanner in=new Scanner(System.in);
+		int num1=in.nextInt();
+		int num2=in.nextInt();
+		double total=0;
+		for (int counter=num1;counter<=num2;counter++) {
 
+			total= total+ counter;
+			System.out.println(total);
+		}
 	}
 
 }
